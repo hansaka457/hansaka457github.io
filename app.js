0eb870483aba46ea6b8ae7bad79e9fba08058005
hansaka457 <hansakadissanayaka916@gmail.com>
2026-06-01 02:28:05 +0530

    Fix JSON structure in app.js

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ const seriesData = {
             {
                 name: "Hunter Doohan",
                 photo:"assets/cast/hunter.jpg"
-            }
-        ]
-   },
+           }
+      ]
+ },
 
     from: {
         title: "FROM",
